7b2c2eba6a538c582708488c64b125dc7b328cc7
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-06-22 09:42:14 -0500

    cuestionary for promt users is done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");  //Inquirer node package
-// const fs = require("fs");   //File module package
-// const  {Triangle, Square, Circle} = require("./lib/shapes");  //Importing figures from shapes.js
+const fs = require("fs");   //File module package
+const  {Triangle, Square, Circle} = require("./lib/shapes");  //Importing figures from shapes.js
+const { type } = require("os");
+const { choices } = require("yargs");
 
 
 
@@ -12,8 +14,27 @@ function promptUser() {
             type: "input",
             message: "What text should appear on your logo? (Enter 3 characteres)",
             name: "text",
-        }
+        },
+        {
+            type: "input",
+            message: "Choose text color",
+            name: "textColor",
+        },
+        {
+            type: "list",
+            message: "What shape would you like to use for your logo?",
+            choices: ["Triangle", "Square", "Circle"],
+            name: "shape",
+        },
+        {
+            type: "input", 
+            message: "Choose shapes color",
+            name: "ShapeBackgroundColor", 
+        },
+
+
     ])
+    
 }
 
 promptUser();
